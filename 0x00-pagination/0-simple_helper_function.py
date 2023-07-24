26d182f660ca0f3cac8ae6777d65add2a4106583
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-24 22:34:06 +0300

    [Chore]: Test task one

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+This module contains part of the
+simple helper function for pagination
+"""
+
+from typing import Tuple
+
+def index_range(page: int, page_size:int) -> Tuple:
+    """.
