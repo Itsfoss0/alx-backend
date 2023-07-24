3785acaee13128de81cbaa907ea220174a5d0ae5
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-24 22:39:30 +0300

    [Chore]: Add task one

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,5 +7,16 @@ simple helper function for pagination
 
 from typing import Tuple
 
-def index_range(page: int, page_size:int) -> Tuple:
-    """.
+return_type = Tuple[int, int]
+
+
+def index_range(page: int, page_size: int) -> return_type:
+    """
+    The Actuall helper function
+    Args:
+        page (int):
+        page_size (int):
+    Returns:
+        Returns an tupple of ints
+    """
+    return ((page - 1) * page_size, page_size * page)
