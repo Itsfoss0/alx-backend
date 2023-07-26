b70d2249b4e91f9271462d5c3463038c15f8d748
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-26 20:05:59 +0300

    [Bug]: Fix task one

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,35 +6,7 @@ This module illustrates how to do simple caching
 in python
 """
 
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("Not implemented")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("Not implemeted")
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
