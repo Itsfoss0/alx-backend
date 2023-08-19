da22e783b64441a01694c6b9b27b34536e04e372
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-19 19:35:44 +0300

    [Chore]: Flask I18N

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+This module holds a simple module to
+learn and practice i18n in flask
+"""
+
+from flask import Flask,  render_template
+from flask_babel import Babel
+
+class Config:
+    """
+    Configure the babel object
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+@app.route("/", strict_slashes=False)
+def hello_holberton():
+    """
+    A simple function that returns
+    Hello world from a flask template
+    Args:
+        None
+    Returns:
+        Returns a flask template
+    ____________________________
+    Example
+        hello_hoberton()
+    """""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
