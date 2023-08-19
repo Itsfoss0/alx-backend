9b0065f499e1209f4b137e3cdf54d5ebfe31fde6
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-19 19:38:10 +0300

    [Chore]: Fix pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,6 +8,7 @@ learn and practice i18n in flask
 from flask import Flask,  render_template
 from flask_babel import Babel
 
+
 class Config:
     """
     Configure the babel object
@@ -15,6 +16,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 babel = Babel(app)
